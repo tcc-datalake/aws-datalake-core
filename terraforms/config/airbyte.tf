3e07b2c9883cfd97dc859351045be267108e57ba
letarzan <leandrorevolve@gmail.com>
2023-09-20 20:31:51 -0300

    fix(airbyte): fix instance type

--- terraforms/config/airbyte.tf
+++ terraforms/config/airbyte.tf
@@ -2,7 +2,7 @@ module "ec2_airbyte" {
   source                    = "../ec2"
   security_group_airbyte_id = module.networking.security_group_airbyte_id
   subnet_id                 = module.networking.subnet_id
-  instance_type             = "t4g.medium"
+  instance_type             = "t3.medium"
   storage_size              = 10
   tag                       = "ec2-${var.name_tag}"
   application_name          = "airbyte"
