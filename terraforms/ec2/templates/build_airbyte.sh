70b6cc782c03dad7b79cdc3d21f4426150784eaa
Luiz Contri <luizcontri.data@gmail.com>
2023-09-30 21:54:50 -0300

    fixed airbyte instance

--- terraforms/ec2/templates/build_airbyte.sh
+++ terraforms/ec2/templates/build_airbyte.sh
@@ -3,5 +3,6 @@ sudo curl -sSL https://get.docker.com/ | sudo sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo mkdir airbyte && cd airbyte
-sudo wget https://raw.githubusercontent.com/airbytehq/airbyte-platform/main/{.env,flags.yml,docker-compose.yaml}
-sudo docker compose up -d
+sudo wget https://raw.githubusercontent.com/airbytehq/airbyte/master/run-ab-platform.sh
+sudo chmod +x run-ab-platform.sh
+sudo ./run-ab-platform.sh -b
